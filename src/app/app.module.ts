7df301ec8eb0107cab7c25277d6223085d30bbb5
Diego <peri.rguez@gmail.com>
2019-03-07 20:19:57 +0100

    reestructuracion src/app lower-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { LowerBarComponent } from './lower-bar/lower-bar.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { MainComponent } from './main/main.component';
+import { LowerBarComponent } from './common/lower-bar/lower-bar.component';
+import { TopBarComponent } from './common/top-bar/top-bar.component';
+import { MainComponent } from './common/main/main.component';
 
 
 @NgModule({
