cf032228662c3ae65ceaeda2061128b789279228
Diego <peri.rguez@gmail.com>
2019-03-10 17:35:52 +0100

    ya funciona esquelas con json

---
+++ src/app/mock-esquelas.ts
@@ -0,0 +1,44 @@
+import {Esquela} from './Esquela';
+
+export const ESQUELAS: Esquela[] = [
+  {
+    user_id: 90,
+    nombre: 'Rodrigo',
+    apellidos: 'Rodriguez Romero',
+    dni: '44412121F',
+    email: 'rrr@gmail.com',
+    fecha_defuncion: Date.now(),
+    fecha_pago: Date.parse('31/10/1991'),
+    descripcion: 'tarara tarara tarara murio el dia tal a las tal y lorem ipsum'
+  },
+  {
+    user_id: 91,
+    nombre: 'Rafael',
+    apellidos: 'Rodriguez Romero',
+    dni: '44412121F',
+    email: 'rrr@gmail.com',
+    fecha_defuncion: Date.now(),
+    fecha_pago: Date.parse('31/10/1991'),
+    descripcion: 'tarara tarara tarara murio el dia tal a las tal y lorem ipsum'
+  },
+  {
+    user_id: 92,
+    nombre: 'Raul',
+    apellidos: 'Rodriguez Romero',
+    dni: '44412121F',
+    email: 'rrr@gmail.com',
+    fecha_defuncion: Date.now(),
+    fecha_pago: Date.parse('31/10/1991'),
+    descripcion: 'tarara tarara tarara murio el dia tal a las tal y lorem ipsum'
+  },
+  {
+    user_id: 93,
+    nombre: 'Roberto',
+    apellidos: 'Rodriguez Romero',
+    dni: '44412121F',
+    email: 'rrr@gmail.com',
+    fecha_defuncion: Date.now(),
+    fecha_pago: Date.parse('31/10/1991'),
+    descripcion: 'tarara tarara tarara murio el dia tal a las tal y lorem ipsum'
+  }
+];
--- src/app/non-common/esquelas/esquelas.component.ts
+++ src/app/non-common/esquelas/esquelas.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Esquela} from '../../Esquela';
+import {ESQUELAS} from '../../mock-esquelas';
 
 @Component({
   selector: 'app-esquelas',
@@ -7,18 +7,8 @@ import {Esquela} from '../../Esquela';
   styleUrls: ['./esquelas.component.css']
 })
 export class EsquelasComponent implements OnInit {
-
-  esquela: Esquela = {
-    user_id: 92,
-    nombre: 'Rodrigo',
-    apellidos: 'Rodriguez Romero',
-    dni: '44412121F',
-    email: 'rrr@gmail.com',
-    fecha_defuncion: Date.now(),
-    fecha_pago: Date.parse('31/10/1991'),
-    descripcion: 'tarara tarara tarara murio el dia tal a las tal y lorem ipsum'
-  };
-
+  esquelas = ESQUELAS;
+  cuantas  = ESQUELAS.length;
   constructor() { }
 
   ngOnInit() {
